dd350543dc2887f1a5a2008a21f960a4c65813a4
Bert Jahn <wepl@whdload.de>
2020-10-02 21:01:29 +0200

    Update container_api.h
    my recommendation for the API

--- container_api.h
+++ container_api.h
@@ -27,6 +27,7 @@ typedef unsigned char uint8_t;
    These are the memory allocation functions that need to be available
    in order to container_api to link.
    Semantics should be the same as in malloc/free (memory from heap)
+   wepl: I don't see why this should be in api.h it may be used container internal and is not api
 */
 
 extern void *container_malloc(uint32_t size);
@@ -61,8 +62,9 @@ extern void container_free(void *ptr);
    Returns:
 	* bytes read if succesfull - it is expected that length bytes is returned i.e. no partial reads
 	* negative values in case of errors - these negative values are passed through on container_* functions are their return values
+   wepl: there is no need for such a function to be provided by whdload
 */
-typedef int (*container_read_func)(void *dest,uint32_t length,uint32_t offset,void *context);
+//typedef int (*container_read_func)(void *dest,uint32_t length,uint32_t offset,void *context);
 
 /*
    Initialize container. Will allocate memory for the directory structure, file list and internal state.
@@ -73,8 +75,12 @@ typedef int (*container_read_func)(void *dest,uint32_t length,uint32_t offset,vo
 	* length - file length for the archive
    Returns:
 	* error code or 0 if success. In case error occured all allocated memory is freed0
+   wepl: I don't understand for what container And context should be needed, container should be sufficient
+   additionally must be the archive to work on referenced, we can do this by filename, filelock or filehandle
+   I probably would prefer a filelock but we can maybe adapt that later, filename is more portable for now maybe
 */
-extern int container_initialize(void **container,void *context,container_read_func read_func,uint32_t length);
+//extern int container_initialize(void **container,void *context,container_read_func read_func,uint32_t length);
+extern int container_initialize(void **container,const char *filename);
 
 /*
    Uninitialize container. frees all memory
@@ -91,8 +97,14 @@ extern int container_uninitialize(void *container);
 	* container - pointer to container
    Returns:
 	* Pointer to null terminated string table container filenames with paths of all the files in the archive
+   wepl: I don't want a filelist. I like that the container iterates over all files in the archive and calls
+   a provided function. The function returns either 0 for terminate, -1 to skip this file or a memory pointer to
+   read the file into.
 */
-extern const char **container_getFileList(void *container);
+//extern const char **container_getFileList(void *container);
+typedef int (*whd_allocFile)(const char *name,uint32_t length);
+extern const char **container_fileCache(void *container,whd_allocFile func);
+
 
 /*
    Get file size. Does not allocate memory
@@ -112,6 +124,8 @@ extern int container_getFileSize(void *container,const char *name);
    Returns:
 	* error code or 0 if success.
 	* error code or 0 for non-compressed, 1 for compressed
+   wepl: ATM this is not needed, it would be only needed if we want support that whdload preloads
+   compressed data, this will not be part of phase 1
 */
 extern int container_isCompressed(void *container,const char *name);
 
@@ -124,8 +138,13 @@ extern int container_isCompressed(void *container,const char *name);
 	* name - file name w. path
    Returns:
 	* error code or 0 if success.
+   wepl: similar to container_fileCache I like to have a function which iterates over
+   all entries in the archive (including (empty) dirs) and calls a whdload function.
+   The function receives a fib for this entry. whdload will copy relevant parts of the fib
+   to own memory and returns a flag to continue=-1 or stop=0
 */
-extern int container_examine(void *dest_fib,void *container,const char *name);
+typedef int (*whd_registerEntry)(const char *path, const FIB *fib);
+extern int container_examine(void *container,whd_registerEntry func);
 
 /*
    Fill in relevant portions 232-byte FIB for a file entry for next item in the directory
@@ -135,6 +154,7 @@ extern int container_examine(void *dest_fib,void *container,const char *name);
 	* container - pointer to container
    Returns:
 	* error code or 0 if success.
+   wepl: this isn't needed
 */
 extern int container_exnext(void *dest_fib,void *container);
 
